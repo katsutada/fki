6591852d9c2cee1421820e17507031e2e0d53942
katsu <miya1movie@gmail.com>
2025-11-27 13:34:20 +0800

    Add files via upload

---
+++ pw.js
@@ -0,0 +1,4 @@
+// pw.js
+const CORRECT_HASHED_PASSWORD = 'e90c64117b960efc1f61864197475f854ed4a682be82e04369a031e40a232f41';
+const LOGIN_STATE_KEY = 'isLoggedIn'; // 儲存在 localStorage 的 key
+const TIMEOUT_KEY = 'lastActivity'; // 儲存在 localStorage 的 key
